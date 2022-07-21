cfd0a47b560ac4d45a6ff045c98f675745f1cdf6
AndrewLitteken <andrew.litteken@gmail.com>
2022-07-21 14:52:09 -0500

    fixing bwt

--- quantumcircuitbenchmarks/qiskit/binary_welded_tree.py
+++ quantumcircuitbenchmarks/qiskit/binary_welded_tree.py
@@ -5,15 +5,16 @@ import random
 def bwt_level_qubits(levels, num_qubits, seed=0xDEADBEEF):
     random.seed(seed)
     pairs = []
+    start = 1
     for l in range(levels):
         if l == 0:
             continue
-        for val in range(0, 2**l):
-            start = 1 if l == 1 else (2**(l - 1) + 1)
+        for val in range(0, 2**l+1):
             current_node = start + val
             half_val = (current_node+1) // 2 - 1
             pairs.append((half_val, current_node))
             pairs.append((num_qubits - 1 - half_val, num_qubits - 1 - current_node))
+        start = current_node
     random.seed(seed)
     random.shuffle(pairs)
     qr = qiskit.QuantumRegister(num_qubits)
@@ -42,8 +43,8 @@ def generate_bwt_max_qubits(max_qubits, seed=0xDEADBEEF):
     n = max_qubits
     levels = 1
     num_qubits = 1
-    while num_qubits < n:
-        if num_qubits + 2**levels + 2**(levels - 1) >= n:
+    while num_qubits <= n:
+        if num_qubits + 2**levels + 2**(levels - 1) > n:
             break
         num_qubits += 2**levels + 2**(levels - 1)
         levels += 1
